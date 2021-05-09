42883cbea4b96153fea75c1134ccb37396365057
Cristian Rojas <cristian@v-labs.fr>
2021-05-09 21:28:57 +0200

    add highlight document on search with jQuery

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,10 +1,129 @@
 loadIndex()
 
+// Highlight with jQuery
+// from: https://stackoverflow.com/questions/41533785/how-to-highlight-search-text-in-html-with-the-help-of-js
+// @todo: This is the only use of jQuery. Find a vanila JS way
+jQuery.fn.highlight = function(pat) {
+
+	function innerHighlight(node, pat) {
+
+		var skip = 0;
+
+		if (node.nodeType == 3) {
+
+			var pos = node.data.toUpperCase().indexOf(pat);
+
+			if (pos >= 0) {
+
+				var spannode       = document.createElement('span');
+				spannode.className = 'highlight';
+				var middlebit      = node.splitText(pos);
+				var endbit         = middlebit.splitText(pat.length);
+				var middleclone    = middlebit.cloneNode(true);
+
+				spannode.appendChild(middleclone);
+				middlebit.parentNode.replaceChild(spannode, middlebit);
+
+				skip               = 1;
+
+			}
+
+		} else if (node.nodeType == 1 && node.childNodes && !/(script|style) /i.test(node.tagName)) {
+			for (var i = 0; i < node.childNodes.length; ++i) {
+
+				i += innerHighlight(node.childNodes[i], pat);
+
+			}
+
+		}
+
+		return skip;
+
+	}
+
+	return this.each(function() {
+
+		innerHighlight(this, pat.toUpperCase());
+
+	});
+
+ };
+
+jQuery.fn.removeHighlight = function() {
+
+	function newNormalize(node) {
+
+		for (var i = 0, children = node.childNodes, nodeCount = children.length; i < nodeCount; i++) {
+
+			var child = children[i];
+
+			if (child.nodeType == 1) {
+
+				newNormalize(child);
+				continue;
+
+			}
+
+			if (child.nodeType != 3) { continue; }
+
+			var next = child.nextSibling;
+
+			if (next == null || next.nodeType != 3) { continue; }
+
+			var combined_text = child.nodeValue + next.nodeValue;
+			new_node          = node.ownerDocument.createTextNode(combined_text);
+
+			node.insertBefore(new_node, child);
+			node.removeChild(child);
+			node.removeChild(next);
+			i--;
+			nodeCount--;
+
+		}
+
+	}
+
+	return this.find("span.highlight").each(function() {
+
+		var thisParent = this.parentNode;
+		thisParent.replaceChild(this.firstChild, this);
+		newNormalize(thisParent);
+
+	}).end();
+
+};
+
+$(function() {
+
+	$('#search-input').bind('keyup change', function(ev) {
+
+		// pull in the new value
+		var searchTerm = $(this).val();
+
+		// remove any old highlighted terms
+		$('body').removeHighlight();
+
+		// disable highlighting if empty
+		if ( searchTerm ) {
+
+			// highlight the new term
+			$('body').highlight( searchTerm );
+
+		}
+
+	});
+
+});
+
+///
+
 var scrollTop = 0
 document.addEventListener("turbolinks:before-render", function() {
   var search_index = document.getElementById("search-results");
   var y = search_index.scrollTop;
   scrollTop = y
+
+ 
 })
 
 document.addEventListener("turbolinks:render", function() {
@@ -15,10 +134,19 @@ document.addEventListener("turbolinks:render", function() {
 document.addEventListener("turbolinks:load", function() {
 	setNoteWrapperState()
 	
-	// window.onload = function() {
-	// // @todo: find current note and select it
-	// }
-
+	
+	const current = window.location.href
+	  
+	 var els = document.getElementsByTagName("a");
+	 for (var i = 0, l = els.length; i < l; i++) {
+		 var el = els[i];
+		
+		 if (el.href === current) {
+			 el.classList.add("selected")
+		 } else {
+			 el.classList.remove("selected")
+		 }
+	 }
 })
 
 function loadIndex() {
@@ -64,18 +192,44 @@ function fetchJSON(callback) {
 }
 
 function performSearch() {
-
+	  
+	  // var body = document.getElementsByClassName('note-wrapper')
+	  // var content = body.innerHTML;
+	  
 	  var input, filter, ul, li, a, i, txtValue;
 	  input = document.getElementById('search-input');
-	  filter = input.value.toUpperCase();
+	  
+	  filter = input.value.toLowerCase();
+	  filter  = filter.replace('/[.*+?^${}()|[\]\\]/g', '\\$&');
+	  
+	  var re = new RegExp(filter, 'g');
+	  
 	  ul = document.getElementById("search-results");
 	  li = ul.getElementsByTagName('li');
 	  
+	  
+	 
+		
+	  
 	  for (i = 0; i < li.length; i++) {
 		a = li[i].getElementsByTagName("a")[0];
+		
 		txtValue = a.textContent || a.innerText;
-		if (txtValue.toUpperCase().indexOf(filter) > -1) {
-		  li[i].style.display = "";
+		if (txtValue.toLowerCase().indexOf(filter) > -1) {
+		  
+		  var content = li[i].innerHTML;
+		  // console.log(content);
+		  if (filter.length > 0) {
+		
+		  }
+		  
+		  
+		  li[i].style.display = "block";
+		 
+		  // if (filter.length > 0) {
+			//   li[i].innerHTML = li[i].innerHTML.replace(re, `<ins>$&</ins>`)
+		  // }
+		  
 		} else {
 		  li[i].style.display = "none";
 		}
